6abb876aeb265f4190d1ebdee1139903d1b11ee6
macsj200 <macsj200@gmail.com>
2013-10-16 16:46:39 -0600

    Comment SizeSelector class

--- JavaPaint/src/javapaint/SizeSelector.java
+++ JavaPaint/src/javapaint/SizeSelector.java
@@ -7,7 +7,11 @@ import javax.swing.JSlider;
 import javax.swing.JTextField;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
-
+/*
+ * I don't want to bother commenting this right now
+ * 
+ * This is not used by anything right now but will be later
+ */
 @SuppressWarnings("serial")
 public class SizeSelector extends JPanel{
 	private JSlider slider = null;
