5667cd6910dee2507a99fdb0faf93f25bfa29ce1
macsj200 <macsj200@gmail.com>
2013-10-16 16:50:02 -0600

    Comment Utensil class

--- JavaPaint/src/javapaint/Utensil.java
+++ JavaPaint/src/javapaint/Utensil.java
@@ -1,15 +1,30 @@
 package javapaint;
-
+/*
+ * Wrapper interface for Shape class
+ * An attempt at polymorphism (I think)
+ * 
+ * This probably isn't necessary
+ */
 import java.awt.Color;
 import java.awt.Shape;
 
 public interface Utensil extends Shape{
 	public void setX(int x);
+	//Set the x coordinate of the utensil
+	
 	public void setY(int y);
+	//Set the y coordinate of the utensil
 	
 	public int[] getCoordinates();
+	//Get an int array with coordinates
+	//getCoordinates()[0] = x
+	//getCoordinates()[1] = y
 	
 	public Utensil clone();
+	//Make a clone of the utensil to put into the elements ArrayList
+	//Probably bad practice
+	
 	public void setColor(Color c);
 	public Color getColor();
+	//Get and set the color of the utensil
 }
