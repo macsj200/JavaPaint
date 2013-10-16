9a29984d6984af5758891e90c770560931a19047
macsj200 <macsj200@gmail.com>
2013-10-16 16:52:04 -0600

    Comment ClearListener class

--- JavaPaint/src/listeners/ClearListener.java
+++ JavaPaint/src/listeners/ClearListener.java
@@ -7,12 +7,16 @@ import javapaint.PaintPanel;
 
 public class ClearListener implements ActionListener{
 	private PaintPanel p = null;
+	//PaintPanel object passed in constructor
+	//Needed to call resetElements()
 
 	public ClearListener(PaintPanel p){
 		this.p = p;
 	}
+	
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		p.resetElements();
+		//On button click (action) clear the PaintPanel
 	}
 }
