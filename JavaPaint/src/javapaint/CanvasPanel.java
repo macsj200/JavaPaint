6ba7b77615198d175c989e6c79cde82b1c7bac8b
macsj200 <macsj200@gmail.com>
2013-10-18 00:01:45 -0600

    Comment CanvasPanel

--- JavaPaint/src/javapaint/CanvasPanel.java
+++ JavaPaint/src/javapaint/CanvasPanel.java
@@ -10,31 +10,35 @@ import javax.swing.JPanel;
 @SuppressWarnings("serial")
 public class CanvasPanel extends JPanel{
 	private Utensil utensil = null;
-	//Utensil object
-	//Used to paint stuff
+	//Currently loaded Utensil object
 	
 	private BufferedCanvas bufferedCanvas = null;
 	//Use a buffered image to render stuff
-	//TODO add threads :)
 
 	CanvasPanel(){
 		this.setPreferredSize(new Dimension(500, 500));
 		bufferedCanvas = new BufferedCanvas(getPreferredSize().width, 
 				getPreferredSize().height, BufferedImage.TYPE_INT_ARGB);
+		//Make a new BufferedImage for to render stuff
 	}
 
 	public void loadUtensil(Utensil utensil){
-		//Set the current utensil
+		//Load in a new utensil
 		this.utensil = utensil;
 	}
 	
 	public void rerender(){
+		//Redraw everything
+		
 		bufferedCanvas.threadedRender();
 		revalidate();
 		repaint();
 	}
 	
 	public void resetElements(){
+		//Clear the canvas of all elements
+		//Empties the ArrayList
+		
 		bufferedCanvas.resetElements();
 		revalidate();
 		repaint();
@@ -46,20 +50,24 @@ public class CanvasPanel extends JPanel{
 	}
 
 	public void paintComponent(Graphics g){
-		//Override paintComponent() method
-		//We can use this to paint special stuff
+		//Override paintComponent() method to paint BufferedCanvas
 		
 		super.paintComponent(g);
 		//Let the normal paintComponent() method do its thing
 		
 		Graphics2D g2d = (Graphics2D) g.create();
+		//Make a new context
 		
 		g2d.drawImage(bufferedCanvas, 0, 0, null);
+		//Write the image
 		
 		g2d.dispose();
+		//We're all done with that
 	}
 	
 	public BufferedCanvas getBufferedCanvas(){
+		//Get a reference to the BufferedCanvas object behind this panel
+		
 		return bufferedCanvas;
 	}
 }
