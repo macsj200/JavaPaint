e2ef64dcb01a5190b4b01d3fd215e624532c755a
macsj200 <macsj200@gmail.com>
2013-10-18 00:19:09 -0600

    Remove TODO comments in MouseDetectorListener

--- JavaPaint/src/listeners/MouseDetectorListener.java
+++ JavaPaint/src/listeners/MouseDetectorListener.java
@@ -30,18 +30,14 @@ public class MouseDetectorListener implements MouseInputListener{
 
 	@Override
 	public void mouseClicked(MouseEvent arg0) {
-
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
-
 	}
 
 	@Override
 	public void mouseExited(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
@@ -52,14 +48,10 @@ public class MouseDetectorListener implements MouseInputListener{
 
 	@Override
 	public void mouseReleased(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void mouseMoved(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
